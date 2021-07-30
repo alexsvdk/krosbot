bab106a48ef50b7394468e56cccf2be4905eba77
a1exs <a1exs@yandex-team.ru>
2021-07-31 01:32:33 +0300

    MultiBotUpdateCreator

--- core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
+++ core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
@@ -1,9 +1,11 @@
 package com.github.alexsvdk.graphit.multibot
 
 import com.github.alexsvdk.graphit.core.ChatNode
+import com.github.alexsvdk.graphit.core.ChatUpdateCreator
 import com.github.alexsvdk.graphit.state.UpdateStateMapper
 import com.github.alexsvdk.graphit.state.StatefulNode
 
 typealias MultiBotNode = ChatNode<MultiBotChatUpdate, MultiBotSender>
+typealias MultiBotUpdateCreator = ChatUpdateCreator<MultiBotChatUpdate, MultiBotSender>
 interface MultiBotStatefulNode<State>: StatefulNode<MultiBotChatUpdate, MultiBotSender, State>
 interface MultiBotStateMapper<State>: UpdateStateMapper<MultiBotChatUpdate, State>

e9f2bd8005540a9b185cc49cbd68f1d5507902ab
a1exs <a1exs@yandex-team.ru>
2021-07-31 04:16:35 +0300

    remove stateful node

--- core/src/main/kotlin/com/github/alexsvdk/graphit/state/UpdateStateMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.github.alexsvdk.graphit.state
-
-import com.github.alexsvdk.graphit.core.ChatUpdate
-
-interface UpdateStateMapper<U: ChatUpdate, State> {
-    fun mapUpdateToState(update: U): State
-}

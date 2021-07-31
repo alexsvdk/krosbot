b7bc16430bf0fca9534c643900bf1c2b67ccc5eb
a1exs <a1exs@yandex-team.ru>
2021-07-31 04:52:21 +0300

    typealias

--- core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
+++ core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
@@ -2,6 +2,8 @@ package com.github.alexsvdk.graphit.multibot
 
 import com.github.alexsvdk.graphit.core.ChatNode
 import com.github.alexsvdk.graphit.core.ChatUpdateCreator
+import com.github.alexsvdk.graphit.graphit.GraphitNode
 
 typealias MultiBotNode = ChatNode<MultiBotChatUpdate, MultiBotSender>
+typealias MultiBotGraphitNode = GraphitNode<MultiBotChatUpdate, MultiBotSender>
 typealias MultiBotUpdateCreator = ChatUpdateCreator<MultiBotChatUpdate, MultiBotSender>

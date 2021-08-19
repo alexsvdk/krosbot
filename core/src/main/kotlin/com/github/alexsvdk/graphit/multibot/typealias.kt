ce1749117ab08f3dbc273cbebdb1c4bfedaae8a7
a1exs <a1exs@yandex-team.ru>
2021-08-19 23:36:58 +0300

    fix

--- core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
+++ core/src/main/kotlin/com/github/alexsvdk/graphit/multibot/typealias.kt
@@ -3,9 +3,7 @@ package com.github.alexsvdk.graphit.multibot
 import com.github.alexsvdk.graphit.core.ChatNode
 import com.github.alexsvdk.graphit.core.ChatUpdateCreator
 import com.github.alexsvdk.graphit.nodes.graphit.GraphitNode
-import com.github.alexsvdk.graphit.nodes.proxy.ProxyNode
 
 typealias MultiBotNode = ChatNode<MultiBotChatUpdate, MultiBotSender>
 typealias MultiBotGraphitNode = GraphitNode<MultiBotChatUpdate, MultiBotSender>
-typealias MultiBotProxyNode = ProxyNode<MultiBotChatUpdate, MultiBotSender>
 typealias MultiBotUpdateCreator = ChatUpdateCreator<MultiBotChatUpdate, MultiBotSender>

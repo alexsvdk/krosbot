a150f3d59e748a71bd43a461de47cc6a893d3e1a
a1exs <a1exs@yandex-team.ru>
2021-08-30 18:10:40 +0300

    PaginationKeyboardBuilder

--- core/src/main/kotlin/com/github/alexsvdk/graphit/core/message/utils/PaginationKeyboardBuilder.kt
+++ core/src/main/kotlin/com/github/alexsvdk/graphit/core/message/utils/PaginationKeyboardBuilder.kt
@@ -1,6 +1,7 @@
 package com.github.alexsvdk.graphit.core.message.utils
 
-import com.github.alexsvdk.graphit.core.message.abstract.KeyboardMessageComponent
+import com.github.alexsvdk.graphit.core.message.KeyboardMessageComponent
+import com.github.alexsvdk.graphit.core.message.OutgoingKeyboard
 
 class PaginationKeyboardBuilder(initializer: (PaginationKeyboardBuilder.() -> Unit)? = null) {
 
@@ -8,19 +9,22 @@ class PaginationKeyboardBuilder(initializer: (PaginationKeyboardBuilder.() -> Un
         initializer?.let { this.it() }
     }
 
+    var keyboardType = KeyboardMessageComponent.Type.KEYBOARD
     var keyboardItems: List<String>? = null
-    var keyboardItemBuilder: ((index: Int) -> String)? = null
+    var keyboardItemBuilder: ((index: Int) -> KeyboardMessageComponent.Button)? = null
     var maxRows: Int = 2
     var maxColumns: Int = 8
-    val totalItems: Int? = null
-    val pageNumber: Int = 0
+    var totalItems: Int? = null
+    var pageNumber: Int = 0
 
-    val nextPageButtonBuilder: ((pageIndex: Int) -> String) = {">"}
-    val prevPageButtonBuilder: ((pageIndex: Int) -> String) = {"<"}
+    var nextPageButtonBuilder: ((pageIndex: Int) -> KeyboardMessageComponent.Button) =
+        { KeyboardMessageComponent.Button(">") }
+    var prevPageButtonBuilder: ((pageIndex: Int) -> KeyboardMessageComponent.Button) =
+        { KeyboardMessageComponent.Button("<") }
 
     val maxItems get() = maxRows * maxColumns
 
-    fun build(): KeyboardMessageComponent {
+    fun build(): OutgoingKeyboard {
         assert(totalItems != null) { "totalItems must not be null" }
         assert(totalItems!! > 2) { "totalItems must not be > 2" }
         assert(pageNumber > 0)
@@ -28,12 +32,27 @@ class PaginationKeyboardBuilder(initializer: (PaginationKeyboardBuilder.() -> Un
         assert(maxColumns > 0)
         assert((keyboardItems != null) xor (keyboardItemBuilder != null)) { "Only one of keyboardItems and keyboardItemBuilder should be passed" }
 
-        val prevItems = if (pageNumber==0) 0 else pageNumber * (maxItems-1) - (pageNumber-1)
-        assert(prevItems < totalItems)
+        var itemsForThisPage = maxItems
+        val prevItems = if (pageNumber == 0) 0 else pageNumber * (maxItems - 2) + 1
+        assert(prevItems < totalItems!!)
 
-        val showPrev = pageNumber>0
+        val showPrev = prevItems > 0
+        if (showPrev) itemsForThisPage--
 
-        TODO()
+        val showNext = totalItems!! - prevItems - itemsForThisPage > 0
+        if (showNext) itemsForThisPage--
+
+        return OutgoingKeyboard(
+            List(maxRows) { row ->
+                List(maxColumns) { col ->
+                    val index = row * maxColumns + col
+                    if (index == 0 && showPrev) prevPageButtonBuilder(pageNumber - 1)
+                    if (index == maxItems - 1 && showNext) nextPageButtonBuilder(pageNumber + 1)
+                    keyboardItemBuilder!!.invoke(prevItems + index)
+                }
+            },
+            keyboardType
+        )
     }
 
 }
